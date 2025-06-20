f2157a490c970efaf4c4d1ae18117dda51cdc98a
aytov66 <hacker.exe2143@gmail.com>
2025-06-20 11:02:47 +0400

    second commit

---
+++ Day 014/Classwork/classwork.py
@@ -0,0 +1,11 @@
+# 1) Create a list of numbers and print it
+numbers = [1, 2, 3, 4, 5]
+print(numbers)
+
+# 2) Create a list with mixed data types and print it
+mixed_data = [42, "hello", 3.14, True]
+print(mixed_data)
+
+# 3) Create any list and get its length
+my_list = ["apple", "banana", "cherry"]
+print(len(my_list))
---
+++ Day 014/Homework/homework.py
@@ -0,0 +1,9 @@
+# The len  function in python is used to return the amount of objectd in an item, for example:
+# len([1, 2, 3, 4, 5])
+# The list function is used to keep multiple objects in the same place, like a container.
+# We can use lists to make our program remember multiple things at once
+# We can store anything in lists, strings, integers, booleans, floats
+
+books = list(['Harry Potter', 'The Lord of the rings', 'The Hobbit'])
+print(books)
+print(len(books))
